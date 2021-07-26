31c940d039a1a8157be9e3806c00ad2d58931b59
Diwanga <diwangaamasith@gmai.com>
2021-07-26 17:26:32 +0530

    change heroku setting

--- server/index.js
+++ server/index.js
@@ -13,9 +13,12 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-
+app.get('/', () => {
+  console.log("MemoryHUB Server");
+})
 const CONNECTION_URL = 'mongodb+srv://diwanga:qwerty123@cluster0.fkusz.mongodb.net/MemoryHUB?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
