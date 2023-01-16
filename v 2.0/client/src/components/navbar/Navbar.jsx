05d99736d0f8424f07171b00e05afff4d670138d
diwangaamasith <diwangaamasith@gmail.com>
2023-01-16 15:20:46 +0530

    adding v 0.2

--- v 2.0/client/src/components/navbar/Navbar.jsx
+++ v 2.0/client/src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="left">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span>lamasocial</span>
+          <span>MemHub</span>
         </Link>
         <HomeOutlinedIcon />
         {darkMode ? (
